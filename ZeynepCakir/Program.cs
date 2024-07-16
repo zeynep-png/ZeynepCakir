bfdbd10d02ad1bcc320d76af8c1847a83c18d16b
Zeynep Çakır <zeey3481.zc@gmail.com>
2024-07-16 11:27:27 +0300

    Proje dosyası ekle.

---
+++ ZeynepCakir/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeynepCakir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zeynep Tuana Çakır \n Yazılım alanında kendimi geliştirip sektörde özgüven kazanabilmek için Patika'nın Bootcamine katıldım. :)");
+        }
+    }
+}
